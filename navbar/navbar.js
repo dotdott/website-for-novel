96472ffd6449256ccc9de362fd9d4ed541c4a406
Gabriel Jodas <70294805+dotdott@users.noreply.github.com>
2021-02-17 14:23:47 -0300

    Update navbar.js

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,8 +1,8 @@
 document.write(
     `<nav>
     <ul>
-    <li><a href='../../../novel-scan/main.html'>Inicio</a></li>
-    <li><a href='../../../novel-scan/contact/contact.html'>Contato</a></li>
+    <li><a href='https://dotdott.github.io/novel-scan/main.html'>Inicio</a></li>
+    <li><a href='https://dotdott.github.io/novel-scan/contact/contact.html'>Contato</a></li>
     </ul>
     </nav>`
 );
